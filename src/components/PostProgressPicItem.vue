5539193bd632ab526b73516fa13a6cb9e627e699
Kirby Choy <kjchoy@ucsc.edu>
2018-10-16 22:34:03 -0700

    Create base components for progress pic
    Base component to create 'progress pic item' and
    base component to display it
    Also separated view components and normal components

---
+++ src/components/PostProgressPicItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-form v-model="valid">
+    <v-text-field
+      v-model="name"
+      :rules="nameRules"
+      :counter="10"
+      label="Name"
+      required
+    ></v-text-field>
+  </v-form>
+</template>
+
+<script>
+
+export default {
+  name: 'post-progress-pic-item'
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container fluid>
+    <v-layout row wrap>
+      <v-flex xs12 class="text-xs-center" mt-5>
+        <h1>Home page</h1>
+      </v-flex>
+      <v-flex xs12 class="text-xs-center" mt-3>
+        <p>This is a user's home page</p>
+      </v-flex>
+
+      <v-flex xs12 class="text-xs-center" mt-3>
+        <post-progress-pic-item>
+        </post-progress-pic-item>
+        
+        <progress-pic-item 
+          v-for='n in 4'
+          :key='n'
+        >
+        </progress-pic-item>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+
+import ProgressPicItem from '../components/ProgressPicItem.vue'
+import PostProgressPicItem from '../components/PostProgressPicItem.vue'
+
+export default {
+  components: {
+    ProgressPicItem,
+    PostProgressPicItem
+  }
+}
+</script>
