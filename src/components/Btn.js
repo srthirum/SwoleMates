93495a42dfba19c43597f294be17d3858d9becd7
ImStackOverflow <34730452+ImStackOverflow@users.noreply.github.com>
2018-10-18 22:55:15 -0700

    added btn file

---
+++ src/components/Btn.js
@@ -0,0 +1,11 @@
+import gapi from 'gapi'
+var GoogleSigninBtn = { gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+      });
+    }
