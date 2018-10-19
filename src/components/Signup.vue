fcbf5080189a35271c0848403f6d22937849217f
ImStackOverflow <34730452+ImStackOverflow@users.noreply.github.com>
2018-10-19 13:27:50 -0700

    widget works for signin

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -48,11 +48,39 @@
         </form>
       </v-flex>
     </v-layout>
+    <v-flex xs12 class="text-xs-center" mt-3>
+      <p style="color:#808080">Or</p>
+      <div id="firebaseui-auth-container"></div>
+    </v-flex>
   </v-container>
 </template>
 
 <script>
+import firebase from 'firebase'
+import firebaseui from 'firebaseui'
 export default {
+  mounted () {
+    // create google signin widget
+    var uiConfig = {
+      signInSuccessUrl: '/home',
+      signInFlow: 'popup',
+      signInOptions: [
+        {
+          // Google provider must be enabled in Firebase Console to support one-tap
+         // sign-up.
+         // Required to enable this provider in one-tap sign-up.
+          authMethod: 'https://accounts.google.com',
+         // Required to enable ID token credentials for this provider.
+         // This can be obtained from the Credentials page of the Google APIs
+         // console.
+          clientId: '816721714419-k26nskknfiqssb8meb7jqo4cjlp4q9qe.apps.googleusercontent.com'
+        },
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+      ]
+    }
+    var ui = new firebaseui.auth.AuthUI(firebase.auth())
+    ui.start('#firebaseui-auth-container', uiConfig)
+  },
   data () {
     return {
       email: '',
